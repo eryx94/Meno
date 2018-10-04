9161d09a8d5e9886abf97e006bb644205cf3563f
Angelo Moroni <mor.angelo.mor@gmail.com>
2018-10-04 19:56:41 +0200

    Initial commit

---
+++ app/src/main/java/com/hooloovoochimico/meno/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.hooloovoochimico.meno
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.ViewGroup
+import android.widget.Button
+import com.crashlytics.android.Crashlytics
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
