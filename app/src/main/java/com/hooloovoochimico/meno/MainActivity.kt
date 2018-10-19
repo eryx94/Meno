71089adb0548470062496ff94ab5fcf8c4bc0922
Alessandro Magaddino (personal account ) <43728482+eryx94@users.noreply.github.com>
2018-10-19 15:54:27 +0200

    Notifydev (#1)
    * i cloned the hooloovoo repo and this is my first commit
    
    * switched in a new branch
    
    * create the memo model and the custom notifier
    
    * first notify done
    
    * ui test implementation
    
    * update
    
    * added the modify button on notify
    
    * added the modify button on notify
    
    * added the modify button on notify
    
    * added the modify button on notify
    
    * substring problem fixed

--- app/src/main/java/com/hooloovoochimico/meno/MainActivity.kt
+++ app/src/main/java/com/hooloovoochimico/meno/MainActivity.kt
@@ -2,10 +2,9 @@ package com.hooloovoochimico.meno
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.ViewGroup
 import android.widget.Button
-import com.crashlytics.android.Crashlytics
-
+import android.widget.EditText
+import com.hooloovoochimico.meno.MenoConstant.EXTRA_MODIFY_ID
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +12,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val title = findViewById<EditText>(R.id.title)
+        val body = findViewById<EditText>(R.id.body)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val note = Notifier(this, 1,Memo(1, title.text.toString(), body.text.toString()))
+            note.showNotify()
+        }
+
+        if(intent.hasExtra(EXTRA_MODIFY_ID)){
+            title.setText("Questo è un evento di modifica xD")
+            val testo = "Questa è l'evento di modifica dalla notifica: ${intent.getIntExtra(EXTRA_MODIFY_ID, 0)}"
+            body.setText(testo)
+        }
+
+
 
 
 
