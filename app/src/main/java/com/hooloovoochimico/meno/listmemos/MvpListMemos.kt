0c0c03566963dcba626e04206e3a1a2fac879040
eryx94 <alex.magaddino@gmail.com>
2018-11-01 12:19:24 +0100

    added coroutines

--- app/src/main/java/com/hooloovoochimico/meno/listmemos/MvpListMemos.kt
+++ app/src/main/java/com/hooloovoochimico/meno/listmemos/MvpListMemos.kt
@@ -2,7 +2,7 @@ package com.hooloovoochimico.meno.listmemos
 
 import android.content.Context
 import com.hooloovoochimico.meno.Memo
-import java.net.ContentHandler
+import kotlinx.coroutines.*
 
 interface IView{
     fun fillAdapter(menos: List<Memo>)
@@ -15,7 +15,35 @@ interface IPresenter{
 }
 
 interface ILogic{
-    fun loadMemos()
+    fun loadMemos(): Deferred<List<Memo>>
 }
 
-class ListMemosPresenter(private val context: Context)
+class ListMemosPresenter(private val context: Context): IPresenter{
+    override fun subscribe(view: IView) {
+        this.view = view
+    }
+
+    override fun unsubscribe() {
+        this.view = null
+    }
+
+    override fun loadMemos() {
+        GlobalScope.launch(Dispatchers.Main) {
+
+            logic.loadMemos().await()
+        }
+    }
+
+    private var view: IView? = null
+    private val logic = ListMemosLogic(context)
+
+}
+
+class ListMemosLogic(private val context: Context): ILogic{
+    override fun loadMemos(): Deferred<List<Memo>> {
+        return GlobalScope.async(Dispatchers.IO) {
+            listOf<Memo>()
+        }
+    }
+
+}
